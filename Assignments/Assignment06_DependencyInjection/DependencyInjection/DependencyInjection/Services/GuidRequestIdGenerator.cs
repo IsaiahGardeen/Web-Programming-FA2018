40d09e0cf7ca3f79273507feccdc246fcbd48c57
Steven Yackel <spazard1@gmail.com>
2018-08-21 16:50:59 -0500

    Adds 2018 repo

---
+++ Assignments/Assignment06_DependencyInjection/DependencyInjection/DependencyInjection/Services/GuidRequestIdGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependencyInjection.Services
+{
+	/*
+	* A class that generates unique request ids.
+	*/
+	public class GuidRequestIdGenerator
+    {
+		public string Generate { get { return Guid.NewGuid().ToString(); } }
+    }
+}
