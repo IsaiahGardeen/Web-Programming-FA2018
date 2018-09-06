4b189eb893140bac228a2a39ea563ceec5339512
Steven Yackel <spazard1@gmail.com>
2018-09-06 14:02:00 -0500

    Adds skeleton for Javascript/JSON lecture

--- Lectures/09-04-2018 Javascript/common.js
+++ Lectures/09-04-2018 Javascript/common.js
@@ -1,30 +1,14 @@
 
-// window allows us to talk to the browser
-
-// alert('test');
-
 
 function doStuff() {
 	
 	
-	document.getElementById("div1").classList.toggle("warning");
-	document.getElementById("div1").classList.toggle("center");
 	
 }
 
 
 
 window.onload = function () {
-	
-	// DOM - document object model
-	// document.getElementById("div1").style.backgroundColor = "blue";
-
-	// a better way, use classes
-	
-	// document.getElementById("div2").classList.add("warning");
-	
-	
-	// access the button by its id and setup the onclick function
 	
 	document.getElementById("doStuffButton").onclick = doStuff;
 	
