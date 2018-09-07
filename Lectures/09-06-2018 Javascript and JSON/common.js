e93257b84f37f85ced445312cdf8a807c18ef2d6
Steven Yackel <spazard1@gmail.com>
2018-09-06 23:30:52 -0500

    Adds javascript and json lecture

--- Lectures/09-06-2018 Javascript and JSON/common.js
+++ Lectures/09-06-2018 Javascript and JSON/common.js
@@ -1,30 +1,58 @@
 
-// window allows us to talk to the browser
-
-// alert('test');
-
 
 function doStuff() {
 	
+	var userInput = document.getElementById("userInput");
+	var results = document.getElementById("results");
 	
-	document.getElementById("div1").classList.toggle("warning");
-	document.getElementById("div1").classList.toggle("center");
+	try {
+		var userJson = JSON.parse(userInput.value);
+	} catch (error) {
+		results.innerHTML = "";
+		displayError(error);
+		
+		return;
+	}
 	
+	displayError("");
+	
+	var ol = document.createElement("ol");
+	
+	if (userJson.userArray) {
+		if (Array.isArray(userJson.userArray)) {
+			userJson.userArray.forEach(function (userElement) {
+				if (typeof userElement === "string") {
+					var li = document.createElement("li");
+					li.appendChild(document.createTextNode(userElement));
+					ol.appendChild(li);
+				} else if (typeof userElement === "number") {
+					var li = document.createElement("li");
+					li.appendChild(document.createTextNode("NUM: " + userElement));
+					ol.appendChild(li);
+				} else {
+					displayError("An element of userArray was not a string or number");
+				}
+				// the type we didn't handle is called "object"
+			});
+			
+			results.appendChild(ol);
+		} else {
+			displayError("userArray key was not an array!");
+		}				
+		
+	} else {
+		displayError("userArray key was undefined!");
+	}
+}
+
+function displayError(error) {
+	
+	document.getElementById("error").innerHTML = error;
 }
 
 
 
 window.onload = function () {
-	
-	// DOM - document object model
-	// document.getElementById("div1").style.backgroundColor = "blue";
-
-	// a better way, use classes
-	
-	// document.getElementById("div2").classList.add("warning");
-	
-	
-	// access the button by its id and setup the onclick function
 	
 	document.getElementById("doStuffButton").onclick = doStuff;
 	
