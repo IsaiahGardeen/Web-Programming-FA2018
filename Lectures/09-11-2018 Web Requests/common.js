b0630281f0e5d69efb37da4bf0586f631860922a
Steven Yackel <spazard1@gmail.com>
2018-09-11 15:59:08 -0500

    Add web requests lecture

--- Lectures/09-11-2018 Web Requests/common.js
+++ Lectures/09-11-2018 Web Requests/common.js
@@ -1,11 +1,70 @@
 var azureUrl = "https://simpleserver2018.azurewebsites.net/api/values";
 
+/*
+
+CRUD - Create, Read, Update, Delete
+
+
+REST - 
+1. Convention over configuration
+2. Stateless
+
+Http Verbs == Http Methods
+	GET - Read data from the server
+	POST - Create data on the server
+	PUT - Update data on the server (and sometimes create)
+	PATCH - Also update
+	DELETE - Delete data from the server
+*/
 
 function runGet() {
-	
-
+	$.ajax(azureUrl,
+	{
+		method: "GET",
+		success: simpleResult,
+		error: simpleError
+	});
 }
 
+function runPost() {
+	
+	$.ajax(azureUrl,
+	{
+		method: "POST",
+		success: simpleResult,
+		error: simpleError,
+		processData: false,
+		contentType: "application/json",
+		data: JSON.stringify({
+			Value: document.getElementById("userInput").value
+		})
+	});
+	
+}
+
+function runPut() {
+	
+	$.ajax(azureUrl + "/" + document.getElementById("userIndex").value,
+	{
+		method: "PUT",
+		success: simpleResult,
+		error: simpleError,
+		processData: false,
+		contentType: "application/json",
+		data: JSON.stringify({
+			Value: document.getElementById("userInput").value
+		})
+	});
+}
+
+function runDelete() {
+	$.ajax(azureUrl + "/" + document.getElementById("userIndex").value,
+	{
+		method: "DELETE",
+		success: simpleResult,
+		error: simpleError
+	});
+}
 
 
 function simpleResult(data) {
@@ -21,5 +80,7 @@ function simpleError(data) {
 window.onload = function () {
 	
 	document.getElementById("runGetButton").onclick = runGet;
-	
+	document.getElementById("runPostButton").onclick = runPost;
+	document.getElementById("runPutButton").onclick = runPut;
+	document.getElementById("runDeleteButton").onclick = runDelete;
 }
