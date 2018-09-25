bd17d580baa3a17ef8520910aef9119198eeb04c
Steven Yackel <spazard1@gmail.com>
2018-09-25 13:52:22 -0500

    Adds base lecture for 9-23

---
+++ Lectures/09-23-2017 WebAPI Part 3 and MVC/Students/Students/Controllers/StudentsController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Students.Entities;
+using Students.Models;
+
+namespace Students.Controllers
+{
+    
+    // this controller will live at the url https://localhost:someport/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        // this is NOT the right way to do this long term.
+        // preview: assignment 6, use dependency injection instead
+        private static List<StudentModel> students = new List<StudentModel>();
+
+        [HttpGet]
+        public IEnumerable<StudentEntity> Get()
+        {
+            return students.Select(studentModel => studentModel.ToEntity());
+        }
+
+        [HttpGet("id:int")]
+        public IActionResult GetOne(int id)
+        {
+            if (id < 0 || id >= students.Count)
+            {
+                return new StatusCodeResult((int)HttpStatusCode.NotFound);
+            }
+
+            var studentEntity = students[id].ToEntity();
+            studentEntity.Views = students[id].Views.Count;
+
+            return new JsonResult(studentEntity);
+        }
+
+        [HttpPost]
+        public StudentEntity Post([FromBody] StudentEntity student)
+        {
+            students.Add(student.ToModel());
+
+            return student;
+        }
+
+        [HttpPut("id:int")]
+        public IActionResult Put([FromBody] StudentEntity student, int id)
+        {
+            if (id < 0 || id >= students.Count)
+            {
+                return new StatusCodeResult((int) HttpStatusCode.NotFound);
+            }
+
+            students[id] = student.ToModel();
+
+            return new JsonResult(student);
+        }
+
+        [HttpPut("id:int")]
+        public IActionResult Patch([FromBody] StudentEntity student, int id)
+        {
+            if (id < 0 || id >= students.Count)
+            {
+                return new StatusCodeResult((int)HttpStatusCode.NotFound);
+            }
+
+            if (student.FirstName != null)
+            {
+                students[id].FirstName = student.FirstName;
+            }
+
+            if (student.LastName != null)
+            {
+                students[id].LastName = student.LastName;
+            }
+
+            return new JsonResult(students[id].ToEntity());
+        }
+
+        [HttpDelete("id:int")]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0 || id >= students.Count)
+            {
+                return new StatusCodeResult((int)HttpStatusCode.NotFound);
+            }
+
+            students.RemoveAt(id);
+
+            return StatusCode((int)HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ Lectures/09-23-2017 WebAPI Part 3 and MVC/Students/Students/Entities/StudentEntity.cs
@@ -0,0 +1,32 @@
+﻿using Students.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Students.Entities
+{
+
+    /*
+     * Entity classes are for sending and receiving data from the user.
+     * NOT for saving into a database
+     */
+    public class StudentEntity
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public int Views { get; internal set; }
+
+        public StudentModel ToModel()
+        {
+            return new StudentModel
+            {
+                FirstName = this.FirstName,
+                LastName = this.LastName
+            };
+        }
+    }
+}
