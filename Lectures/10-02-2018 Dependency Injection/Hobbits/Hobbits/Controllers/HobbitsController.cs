6ee5df18fac0790d80041a665eb9a01290055126
Steven Yackel <spazard1@gmail.com>
2018-10-01 15:15:31 -0500

    Added base lecture for Dependency Injection and fixed wrong years

---
+++ Lectures/10-02-2018 Dependency Injection/Hobbits/Hobbits/Controllers/HobbitsController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Hobbits.Entities;
+using Hobbits.Services;
+using System.Net;
+
+namespace Hobbits.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HobbitsController : Controller
+    {
+        private static HobbitDatabase hobbits = new HobbitDatabase();
+
+        [HttpGet]
+        public IEnumerable<HobbitEntity> Get()
+        {
+            return hobbits.Get().Select(hm => hm.ToEntity());
+        }
+
+        [HttpGet("{id}")]
+        public HobbitEntity Get(int id)
+        {
+            return hobbits.Get(id).ToEntity();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]HobbitEntity hobbit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "Check to ensure the structure of your JSON hobbit."
+                };
+            }
+
+            if (hobbits.Add(hobbit.ToModel()))
+            {
+                return Json(hobbit);
+            }
+            else
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "Could not add your hobbit"
+                };
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]HobbitEntity hobbit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "Check to ensure the structure of your JSON hobbit."
+                };
+            }
+
+            if (hobbits.Add(hobbit.ToModel(), id))
+            {
+                return Json(hobbit);
+            }
+            else
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "Your hobbit index was invalid."
+                };
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            hobbits.Delete(id);
+        }
+    }
+}
