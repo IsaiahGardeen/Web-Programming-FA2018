a320d912f6944cb27b7faac0e4202430182732eb
Steven Yackel <spazard1@gmail.com>
2018-10-11 12:28:59 -0500

    Add base lecture for async/await

---
+++ Lectures/10-11-2018 async await/Hobbits/Hobbits/Controllers/HobbitsController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Hobbits.Entities;
+using Hobbits.Services;
+using System.Net;
+using Hobbits.Filters;
+
+namespace Hobbits.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [TypeFilter(typeof(RequestIdFilter))]
+    public class HobbitsController : Controller
+    {
+        private LoggingService loggingService;
+
+        private HobbitDatabase hobbitDatabase;
+
+        private IRequestIdGenerator requestIdGenerator;
+
+        public HobbitsController(LoggingService loggingService, HobbitDatabase hobbitDatabase, IRequestIdGenerator requestIdGenerator)
+        {
+            this.loggingService = loggingService;
+            this.hobbitDatabase = hobbitDatabase;
+            this.requestIdGenerator = requestIdGenerator;
+        }
+
+        [HttpGet]
+        public IEnumerable<HobbitEntity> Get()
+        {
+            loggingService.Log("GET all hobbits");
+
+            return hobbitDatabase.Get().Select(hm => hm.ToEntity());
+        }
+
+        [HttpGet("{id}")]
+        public HobbitEntity Get(int id)
+        {
+            loggingService.Log($"GET one hobbit {id}");
+
+            return hobbitDatabase.Get(id).ToEntity();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]HobbitEntity hobbit)
+        {
+            loggingService.Log("Created a new hobbit");
+
+            if (!ModelState.IsValid)
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "Check to ensure the structure of your JSON hobbit."
+                };
+            }
+
+            if (hobbitDatabase.Add(hobbit.ToModel()))
+            {
+                return Json(hobbit);
+            }
+            else
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "Could not add your hobbit"
+                };
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]HobbitEntity hobbit)
+        {
+            loggingService.Log("Created a new hobbit");
+
+            if (!ModelState.IsValid)
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "Check to ensure the structure of your JSON hobbit."
+                };
+            }
+
+            if (hobbitDatabase.Add(hobbit.ToModel(), id))
+            {
+                return Json(hobbit);
+            }
+            else
+            {
+                return new ContentResult()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Content = "Your hobbit index was invalid."
+                };
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            loggingService.Log("Deleted a hobbit");
+
+            hobbitDatabase.Delete(id);
+        }
+    }
+}
---
+++ Lectures/10-11-2018 async await/Hobbits/Hobbits/Services/DatabaseAccess.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Hobbits.Services
+{
+    public class DatabaseAccess
+    {
+        RetryPolicy rp = new RetryPolicy(new DetectionStrategy(), 5);
+
+        public string Get()
+        {
+
+            var result = rp.ExecuteAction(() =>
+            {
+                var request = WebRequest.Create("https://www.google.com");
+                request.Method = "GET";
+                var response = request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                return responseString;
+            });
+
+            rp.Retrying += (sender, args) =>
+            {
+                Console.WriteLine("We are trying again.");
+            };
+
+            // here if the responseString was JSON (like it will be in assignment 7), we could call
+            // Json.Parse(responseString);
+
+            return result;
+        }
+    }
+}
