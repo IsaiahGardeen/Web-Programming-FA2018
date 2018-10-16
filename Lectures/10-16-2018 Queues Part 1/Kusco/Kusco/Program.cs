6d59163980189812cbcdc7cc336107cd50d92110
Steven Yackel <spazard1@gmail.com>
2018-10-16 15:48:22 -0500

    Adds lecture code for Queues Part 1

---
+++ Lectures/10-16-2018 Queues Part 1/Kusco/Kusco/Program.cs
@@ -0,0 +1,49 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace Kusco
+{
+    class Program
+    {
+        private static BasicAWSCredentials credentials;
+        private static AmazonSQSClient sqs;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting to read messages...");
+
+            credentials = new BasicAWSCredentials("keyid", "key");
+            sqs = new AmazonSQSClient(credentials, RegionEndpoint.USEast1);
+
+            ReadMessagesAsync().Wait();
+        }
+
+        public async static Task ReadMessagesAsync()
+        {
+            var request = new ReceiveMessageRequest();
+            request.MaxNumberOfMessages = 10;
+            request.WaitTimeSeconds = 10;
+            request.QueueUrl = "https://sqs.us-east-1.amazonaws.com/570108184261/webprogramminglecture";
+
+            while (true) {
+                var messages = await sqs.ReceiveMessageAsync(request);
+
+                foreach (var message in messages.Messages)
+                {
+                    Console.WriteLine(message.Body);
+
+                    await sqs.DeleteMessageAsync(new DeleteMessageRequest()
+                    {
+                        QueueUrl = "https://sqs.us-east-1.amazonaws.com/570108184261/webprogramminglecture",
+                        ReceiptHandle = message.ReceiptHandle
+                    });
+                }
+            }
+        }
+    }
+}
